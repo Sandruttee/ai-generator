7600ec22cbee4bd4fe6b44ce748fa553a77cd0d5
Sandruttee <sandra.placenyte@gmail.com>
2024-02-11 18:59:37 +0100

    Updated context for ai

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateRecipe(event) {
   let apiKey = "1be70t3b32a048fab4oc2947a8669607";
 
   let context =
-    "You are an expert in food making and know a lot of delicious food recipes that you are eager to share with the users. Your mission is to generate recipe that includes all the ingredients needed and also all the instructions how to make it. ";
+    "You are an expert in food making and know a lot of delicious food recipes that you are eager to share with the users. Your mission is to generate recipe in basic HTML and seperate each line with a <br/>. Seperate each ingredients with a <br/>. Recipe at the start includes all the ingredients needed for the recipe. The ingredients must be listed. Use food related emoji at the start of each ingredient. The second part should be instructions how to make it. ";
   let prompt = `Generate a recipe for ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
